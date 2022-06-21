cb3744588f88775200c2c9c05d19ec5fc5b710bc
zoshivj <zoshivj@gmail.com>
2022-06-21 23:46:09 +0530

    jitsi

--- app/src/main/java/com/example/lyka/MainActivity.java
+++ app/src/main/java/com/example/lyka/MainActivity.java
@@ -4,11 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            URL serverUrl = new URL("https://meet.jit.si");
+            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                    .setServerURL(serverUrl.getHost())
+                    .setRoom("test123")
+                    .setAudioMuted(false)
+                    .setVideoMuted(false)
+                    .setAudioOnly(false)
+                    .setWelcomePageEnabled(false)
+                    .setConfigOverride("requireDisplayName", true)
+                    .build();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
