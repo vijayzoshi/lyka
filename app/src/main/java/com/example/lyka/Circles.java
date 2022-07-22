cc6ddf73fcd00d8d3a6556a80874fa5319038269
zoshivj <zoshivj@gmail.com>
2022-07-22 23:16:34 +0530

    new

---
+++ app/src/main/java/com/example/lyka/Circles.java
@@ -0,0 +1,22 @@
+package com.example.lyka;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Circles extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+         View view = inflater.inflate(R.layout.fragment_circles, container, false);
+         return view;
+    }
+}
--- app/src/main/java/com/example/lyka/MainActivity.java
+++ app/src/main/java/com/example/lyka/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.lyka;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -14,7 +20,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
+
+
+     /*   try {
             URL serverUrl = new URL("https://meet.jit.si");
             JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
                     .setServerURL(serverUrl.getHost())
@@ -30,18 +38,47 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+*/
 
 
+        NavigationBarView bottomnav = findViewById(R.id.bottom_navigation_view);
+        bottomnav.setOnItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home()).commit();
 
 
-
-
-
-
-
-
-
-
-
+        
     }
+
+
+    private NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull final MenuItem menuItem) {
+
+
+            Fragment selectedfragment = null;
+            switch (menuItem.getItemId()) {
+
+                case R.id.home:
+                    selectedfragment = new Home();
+                    break;
+
+                case R.id.circles:
+                    selectedfragment = new Circles();
+                    break;
+
+                case R.id.community:
+                    selectedfragment = new Community();
+                    break;
+
+
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedfragment).commit();
+
+
+            return true;
+        }
+    };
+
 }
